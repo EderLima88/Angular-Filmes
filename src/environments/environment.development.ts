3619c3feaf3f630acd3ca84de22e5259bffb74e0
Eder de Lima <eder.ebl@gmail.com>
2024-02-08 09:57:06 -0300

    Estilizado cor de fundo

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,5 +3,6 @@
 export const environment = {
     production: false,
     apiKey: 'aa2458d0d530b8ca5a1960cbf1dc513e',
-    apiUrl: 'http://api.themoviedb.org/3/discover/movie'
+    apiUrl: 'http://api.themoviedb.org/3/discover/movie',
+    imgUrl: 'https://image.tmdb.org/t/p/w500/'
 };
